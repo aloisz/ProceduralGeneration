79de4719664afc6682d02003b5f1c6f5429c363c
aloisz <alois.tupinon@gmail.com>
2024-12-05 17:12:36 +0100

    random planet rotation

--- GenPro_Project/Assets/Script/RotatingPlanet.cs
+++ GenPro_Project/Assets/Script/RotatingPlanet.cs
@@ -1,16 +1,18 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RotatingPlanet : MonoBehaviour
 {
     private Quaternion baseRotation;
-    [SerializeField] private Vector3 rotatingVector = Vector3.up;
-    [SerializeField] private float speed = 10;
-
+    private Vector3 rotatingVector = Vector3.up;
+    private float speed = 10;
 
     private void Start()
     {
         baseRotation = transform.rotation;
+        rotatingVector = Random.onUnitSphere;
+        speed = Random.Range(2, 15);
     }
 
     private void Update()
@@ -20,6 +22,8 @@ public class RotatingPlanet : MonoBehaviour
 
     public void ResetRotation()
     {
+        rotatingVector = Random.onUnitSphere;
+        speed = Random.Range(2, 15);
         transform.rotation = baseRotation;
     }
 }
